784d0256d7bfcddb935f9ae8822c20b1b58f0c4e
Esnaux-Virgile <virgile.esnaux@edu.univ-fcomte.fr>
2026-03-09 16:35:06 +0100

    Page d'accueil réalisé à moitié

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,3 +1,14 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090/');
+export const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL ?? 'http://127.0.0.1:8090');
+
+export const getArtistes = () => pb.collection('artistes').getFullList({ sort: 'name' });
+export const getArtisteById = (id) => pb.collection('artistes').getOne(id);
+export const getScenes = () => pb.collection('scenes').getFullList({ sort: 'name' });
+export const getSceneById = (id) => pb.collection('scenes').getOne(id, { expand: 'artistes' });
+export const getPartenaires = () => pb.collection('partenaires').getFullList();
+export const getMembres = () => pb.collection('membres').getFullList({ sort: 'groupe' });
+
+export const isLoggedIn = () => pb.authStore.isValid;
+export const login = (email, password) => pb.collection('users').authWithPassword(email, password);
+export const logout = () => pb.authStore.clear();
